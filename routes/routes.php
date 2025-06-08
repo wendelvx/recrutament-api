7cfe14b974972eebc28beaa09bd8943b1d4f0e6e
wendel <capauzumaki@gmail.com>
2025-06-08 15:16:11 -0300

    Adicionando arquivo de rotas

---
+++ routes/routes.php
@@ -0,0 +1,33 @@
+<?php
+require_once __DIR__ . '/../controllers/PessoaController.php';
+require_once __DIR__ . '/../controllers/VagaController.php';
+require_once __DIR__ . '/../controllers/CandidaturaController.php';
+
+
+$uri = str_replace('/recrutamento-api/public', '', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+if (preg_match('#/pessoas$#', $uri) && $method === 'POST') {
+    $controller = new PessoaController();
+    $controller->criarPessoa();
+    exit;
+}
+
+if (preg_match('#/vagas$#', $uri) && $method === 'POST') {
+    $controller = new VagaController();
+    $controller->criarVaga();
+    exit;
+}
+
+if (preg_match('#/candidaturas$#', $uri) && $method === 'POST') {
+    $controller = new CandidaturaController();
+    $controller->criarCandidatura();
+    exit;
+}
+
+
+// Caso não encontre rota
+http_response_code(404);
+echo json_encode(["erro" => "Endpoint não encontrado"]);
+?>
